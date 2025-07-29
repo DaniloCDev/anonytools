71a39a294beec5b41fb87dfa717351b90e9f7755
DaniloCDev <loppesofc@gmail.com>
2025-07-29 00:18:45 -0300

    add

--- Backend/src/external/dataimpulse/getUser.ts
+++ Backend/src/external/dataimpulse/getUser.ts
@@ -2,6 +2,6 @@ import { dataImpulseClient } from './client';
 
 export async function getUser(subId :Number ): Promise<any> {
     
-    const { data } = await dataImpulseClient.get(`reseller/sub-user/get?subuser_id=${subId}`);
+    const { data } = await dataImpulseClient.get(`/reseller/sub-user/get?subuser_id=${subId}`);
     return data;
 }
