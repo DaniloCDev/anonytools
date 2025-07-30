7100a4f87d4a1849cb9bf5f4d9c741f2800e3b23
DaniloCDev <loppesofc@gmail.com>
2025-07-29 22:27:05 -0300

    WIP: alterações locais no package

--- Backend/src/services/user.proxy.services.ts
+++ Backend/src/services/user.proxy.services.ts
@@ -70,13 +70,11 @@ class ProxyUserService {
         if (!user) throw new Error("Usuário não encontrado.");
 
         const descUser = await this.userRepository.getSubuserIdByUserId(userId);
-        console.log(descUser)
-
         const newBlockedStatus = !user.blocked;
         await this.userRepository.toggleUserBlock(userId, newBlockedStatus);
 
         const respBalance = await blockSubUser(Number(descUser?.subuserId), newBlockedStatus);
-      //  if (!respBalance.blocked) throw new Error("Não foi possível alterar o status do usuário.");
+        if (respBalance.blocked == newBlockedStatus) throw new Error("Não foi possível alterar o status do usuário.");
 
         return respBalance.blocked;
     }
