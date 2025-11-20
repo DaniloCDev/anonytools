7eb087f63c2f76eee562f747d5ef21b06d2d1b75
DaniloCDev <loppesofc@gmail.com>
2025-11-20 15:07:37 -0300

    only the backend

--- Web/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <link rel="icon" href="/favicon.ico" />
-      <title>Nox24Proxy</title>
-      <meta name="description" content="Sua descrição aqui" />
-    </>
-  )
-}
--- Web/next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://nox24proxy.com.br',
-  generateRobotsTxt: true, // opcional: também gera o robots.txt
-};
